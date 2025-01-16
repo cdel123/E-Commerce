399208af1a237ba895c95e1715a9945489489395
Saurabh <superstarsaurabh17@gmail.com>
2025-01-16 11:09:02 +0530

    removed prod routing links from server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,7 @@ import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
 app.use('/api/products', productRoutes);
 app.get('/', (req, res) => {
   res.send('API is running...');
