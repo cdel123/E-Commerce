a5930a23b4fa84f4a1f80c4fed7284a920620f61
Saurabh <superstarsaurabh17@gmail.com>
2025-01-20 12:00:08 +0530

    Register user and generateToken util

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -76,5 +76,18 @@ const orderSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const Order = mongoose.model('Order', orderSchema);
 export default Order;
