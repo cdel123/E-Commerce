670de79c35a461bdc2d14a50f550de4aa9d2c4b5
Saurabh <superstarsaurabh17@gmail.com>
2025-01-20 09:25:52 +0530

    User auth endpoint, routes and controller

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,18 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
