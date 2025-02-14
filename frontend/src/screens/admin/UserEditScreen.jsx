3fc1b26738ad25fbc998fd6741d3d46e448799db
Saurabh <superstarsaurabh17@gmail.com>
2025-02-14 15:16:31 +0530

    small changes

--- frontend/src/screens/admin/UserEditScreen.jsx
+++ frontend/src/screens/admin/UserEditScreen.jsx
@@ -48,6 +48,7 @@ const UserEditScreen = () => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
         {isLoading ? (
           <Loader />
         ) : error ? (
